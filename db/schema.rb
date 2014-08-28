5e2bcefebfb4d11345ad2f64247b248c1e9b8763
Will <wmitchel@hotmail.com>
2014-08-27 18:34:13 -0700

    branding change

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826230542) do
-
+ActiveRecord::Schema.define(version: 20140827144511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
